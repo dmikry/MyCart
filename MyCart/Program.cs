63d6d1cc31614a8772cbcd93871ca185713629d0
Dmytro K <carlos.dk87@gmail.com>
2023-05-12 00:13:00 +0300

    my commit

---
+++ MyCart/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace MyCart
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			Console.WriteLine("Hello, World!");
+		}
+	}
+}
---
+++ MyCartTests/UnitTest1.cs
@@ -0,0 +1,16 @@
+namespace MyCartTests
+{
+	public class Tests
+	{
+		[SetUp]
+		public void Setup()
+		{
+		}
+
+		[Test]
+		public void Test1()
+		{
+			Assert.Pass();
+		}
+	}
+}
