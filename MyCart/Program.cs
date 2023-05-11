59cda3b211ebcc0dab0c5134fc56786e2264a3a2
Dmytro K <carlos.dk87@gmail.com>
2023-05-12 00:27:52 +0300

    first commit

--- MyCart/Program.cs
+++ MyCart/Program.cs
@@ -1,10 +1,82 @@
-﻿namespace MyCart
+﻿namespace MyCart;
+
+using System;
+using System.Collections.Generic;
+
+public class Program
 {
-	internal class Program
+	static void Main(string[] args)
 	{
-		static void Main(string[] args)
+		MyCart cart = new MyCart(123);
+		cart.AddItem("Item A", 2, 15.0m);
+		cart.AddItem("Item B", 1, 10.0m);
+		Console.WriteLine("Cart Amount: " + cart.CartAmount);
+		cart.RemoveItem("Item B");
+		Console.WriteLine("Cart Amount: " + cart.CartAmount);
+	}
+}
+
+public class MyCart
+{
+	private readonly int userId;
+	private readonly Dictionary<string, CartItem> items;
+
+	public MyCart(int userId)
+	{
+		this.userId = userId;
+		items = new Dictionary<string, CartItem>();
+	}
+
+	public decimal CartAmount
+	{
+		get
 		{
-			Console.WriteLine("Hello, World!");
+			decimal total = 0;
+			foreach (var item in items)
+			{
+				total += item.Value.Quantity * item.Value.Price;
+			}
+			return total;
 		}
+	}
+
+	public void AddItem(string itemName, int quantity, decimal price)
+	{
+		if (price <= 0)
+		{
+			throw new ArgumentException("The price should be positive.", nameof(price));
+		}
+
+		if (items.ContainsKey(itemName))
+		{
+			var currentQuantity = items[itemName].Quantity;
+			items[itemName] = new CartItem(itemName, currentQuantity + quantity, price);
+		}
+		else
+		{
+			items[itemName] = new CartItem(itemName, quantity, price);
+		}
+	}
+
+	public void RemoveItem(string itemName)
+	{
+		if (items.ContainsKey(itemName))
+		{
+			items.Remove(itemName);
+		}
+	}
+}
+
+public class CartItem
+{
+	public string Name { get; }
+	public int Quantity { get; }
+	public decimal Price { get; }
+
+	public CartItem(string name, int quantity, decimal price)
+	{
+		Name = name;
+		Quantity = quantity;
+		Price = price;
 	}
 }
--- MyCartTests/UnitTest1.cs
+++ MyCartTests/UnitTest1.cs
@@ -1,16 +1,61 @@
-namespace MyCartTests
-{
-	public class Tests
-	{
-		[SetUp]
-		public void Setup()
-		{
-		}
+namespace MyCartTests;
 
-		[Test]
-		public void Test1()
-		{
-			Assert.Pass();
-		}
+using NUnit.Framework;
+using System;
+using MyCart;
+
+[TestFixture]
+public class MyCartTests
+{
+	private MyCart cart;
+
+	[SetUp]
+	public void Setup()
+	{
+		cart = new MyCart(123);
+	}
+
+	[Test]
+	public void AddItem_ShouldIncreaseCartAmount()
+	{
+		// Arrange
+		decimal initialAmount = cart.CartAmount;
+
+		// Act
+		cart.AddItem("Item A", 2, 15.0m);
+
+		// Assert
+		decimal updatedAmount = cart.CartAmount;
+		Assert.That(updatedAmount, Is.GreaterThan(initialAmount));
+	}
+
+	[Test]
+	public void RemoveItem_ShouldDecreaseCartAmount()
+	{
+		// Arrange
+		cart.AddItem("Item A", 2, 15.0m);
+		decimal initialAmount = cart.CartAmount;
+
+		// Act
+		cart.RemoveItem("Item A");
+
+		// Assert
+		decimal updatedAmount = cart.CartAmount;
+		Assert.That(updatedAmount, Is.LessThan(initialAmount));
+	}
+
+	[Test]
+	public void CartAmount_ShouldCalculateCorrectly()
+	{
+		// Arrange
+		cart.AddItem("Item A", 2, 15.0m);
+		cart.AddItem("Item B", 1, 10.0m);
+
+		// Act
+		decimal cartAmount = cart.CartAmount;
+
+		// Assert
+		decimal expectedAmount = 2 * 15.0m + 1 * 10.0m;
+		Assert.That(cartAmount, Is.EqualTo(expectedAmount));
 	}
 }
